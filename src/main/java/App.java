347c46fcc03c8d5aa16c59d847352da970778b2d
Alex <alexrosenk@hotmail.se>
2023-10-24 21:37:32 +0200

    Added some initial code to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
 // Placeholder
+import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
-        // code
+        // https://jdbc.postgresql.org/documentation/use/#connection-parameters
+        // url form is a string: jdbc:postgresql://host:port/database
+        // host defaults to localhost, see my notes/psql docs for more info
+        // port defaults to 5432
+        // database name in my case appdatabase, default would be OS username used to connect though
+        //Connection db = DriverManager.getConnection(url, username, password)
     }
 }
