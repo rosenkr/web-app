36626bf06698de6b379d46cef860d34d4218043d
Alex <alexrosenk@hotmail.se>
2023-10-25 18:09:59 +0200

    Modified App.java to be able to test if gradle project build was successful. The code now, given my actual database password as command-line argument, successfully connects to my local database "appdatabase" through the user "sasha".

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,23 @@
 // Placeholder
 import java.util.*;
 import java.sql.*;
-import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
+        // by default gradle does not work with interactive command line stdin so,
+        // instead, ask for command-line argument for testing purposes 
+        if (args.length < 1) {
+            System.err.println("Please provide the database password as a command-line argument.");
+            System.exit(1);
+        }
+
+        String password = args[0];        
+        try {
+            Connection connection = new App().getConnection(password);
+            // Test database operations, confirm successful operations in psql
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }        
         // https://jdbc.postgresql.org/documentation/use/#connection-parameters
         // url form is a string: jdbc:postgresql://host:port/database
         // host defaults to localhost, see my notes/psql docs for more info
@@ -13,12 +26,12 @@ public class App {
         //Connection db = DriverManager.getConnection(url, username, password)
     }
     // use https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html setProperty method instead of put method from https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html for good practice.
-    public Connection getConnection() throws SQLException {
+    public Connection getConnection(String password) throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/appdatabase";
         Connection conn = null;
         Properties props = new Properties();
         props.setProperty("user", "sasha");
-        props.setProperty("password", "temp");
+        props.setProperty("password", password);
         conn = DriverManager.getConnection(url, props);
         System.out.println("Connected to database");
         return conn;
