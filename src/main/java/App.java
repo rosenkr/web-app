769b87a056bdafe746d42fc17c783740180aaa54
Alex <alexrosenk@hotmail.se>
2023-10-25 16:23:26 +0200

    Cleaned up the project directory by removing unnecessary files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 // Placeholder
+import java.util.*;
 import java.sql.*;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
@@ -9,5 +11,16 @@ public class App {
         // port defaults to 5432
         // database name in my case appdatabase, default would be OS username used to connect though
         //Connection db = DriverManager.getConnection(url, username, password)
+    }
+    // use https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html setProperty method instead of put method from https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html for good practice.
+    public Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/appdatabase";
+        Connection conn = null;
+        Properties props = new Properties();
+        props.setProperty("user", "sasha");
+        props.setProperty("password", "temp");
+        conn = DriverManager.getConnection(url, props);
+        System.out.println("Connected to database");
+        return conn;
     }
 }
